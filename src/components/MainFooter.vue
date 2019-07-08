5eb1d7a6cfd96a440d71a42aedebd2f5719d94e9
석주연 <juyeonsg@gmail.com>
2019-07-08 17:50:15 +0900

    footer weather

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,6 +1,12 @@
 <template>
   <v-footer height="auto" color="indigo" dark>
           <v-layout justify-center row wrap>
+            <!--weather-->
+            <v-layout>
+              <v-flex xs12>
+                <a class="weatherwidget-io" href="https://forecast7.com/en/36d12128d34/gumi-si/" data-label_1="GUMI" data-label_2="WEATHER" data-theme="weather_one" >GUMI WEATHER</a>
+              </v-flex>
+            </v-layout>
             <v-flex color="indigo" dark py-3 text-xs-center white--text xs12>
               &copy;2019 — <strong>Vuetify</strong>
             </v-flex>
@@ -14,6 +20,16 @@ export default {
     drawer: null
   })
 };
+!function(d,s,id){
+  var js,fjs=d.getElementsByTagName(s)[0];
+  if(!d.getElementById(id)){
+    js=d.createElement(s);
+    js.id=id;
+    js.src='https://weatherwidget.io/js/widget.min.js';
+    fjs.parentNode.insertBefore(js,fjs);
+  }
+}(document,'script','weatherwidget-io-js');
+
 </script>
 
 <style scoped>
