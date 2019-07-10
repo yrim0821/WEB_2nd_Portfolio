b9b794ffcabd5188274533f73eddc38eea88e23b
김영훈 <kyh2021@naver.com>
2019-07-10 10:14:20 +0900

    0710AM

--- src/components/ImgBanner.vue
+++ src/components/ImgBanner.vue
@@ -1,31 +1,83 @@
 <template>
   <div>
-    <v-parallax :src="imgSrc" aspect-ratio="16/4" height="720">
+    <v-img :src="imgSrc"
+           aspect-ratio="1.7">
       <v-layout align-center justify-center row fill-height>
         <v-flex text-xs-center>
           <span class="text-shadow display-2 font-weight-light">
             <slot name="text"/>
           </span>
-          <em>ssafy vue web-mobile project</em>
         </v-flex>
       </v-layout>
-    </v-parallax>
+    </v-img>
+    <form id="imgur">
+      <input type="file" class="imgur" accept="image/*" data-max-size="5000"/>
+    </form>
   </div>
 </template>
 
-
-
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
 <script>
-
+import $ from 'jquery'
 export default {
-	name: 'ImgBanner',
-	props: {
-		imgSrc: {type: String},
-		text: {type: String}
-	},
-	methods: {
-	},
+  name: 'App',
+   name: 'ImgBanner',
+   props: {
+      imgSrc: {type: String},
+      text: {type: String}
+   },
+   methods: {
+   },
 }
+$("document").ready(function() {
+
+  $('input[type=file]').on("change", function() {
+
+    var $files = $(this).get(0).files;
+
+    if ($files.length) {
+
+      // Reject big files
+      if ($files[0].size > $(this).data("max-size") * 1024) {
+        console.log("Please select a smaller file");
+        return false;
+      }
+
+      // Begin file upload
+      console.log("Uploading file to Imgur..");
+
+      // Replace ctrlq with your own API key
+      var apiUrl = 'https://api.imgur.com/3/image';
+      var apiKey = '36757703a6732ed';
+
+      var settings = {
+        async: false,
+        crossDomain: true,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        url: apiUrl,
+        headers: {
+          Authorization: 'Client-ID ' + apiKey,
+          Accept: 'application/json'
+        },
+        mimeType: 'multipart/form-data'
+      };
+
+      var formData = new FormData();
+      formData.append("image", $files[0]);
+      settings.data = formData;
+
+      // Response contains stringified JSON
+      // Image URL available at response.data.link
+      $.ajax(settings).done(function(response) {
+        console.log(response);
+      });
+
+    }
+  });
+
+});
 </script>
 <style>
   .text-shadow {
